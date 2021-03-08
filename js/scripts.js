5eaa85adf01d1fd75cb25be759aa78cc64467836
joankamere <joankamere@gmail.com>
2021-03-08 18:20:03 +0300

    add toppings code to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,33 @@
 $(document).ready(function(){
-    
+    function mushrooms() {
+        var cheese = 0;
+        var addCheese = document.getElementById("toppings");
+        if (addCheese.checked === true) {
+            cheese = 50;
+        }
+        return parseInt(cheese);
+    }
+    function pepperoni() {
+        var pepperoni = 0;
+        var addPepperoni = document.getElementById("toppings");
+        if (addPepperoni. checked === true) {
+            pepperoni = 80;
+        }
+        return parseInt(pepperoni);
+    }
+    function mushrooms() {
+        var mushroom = 0;
+        var addMushroom = document.getElementById("toppings");
+        if (addMushroom.checked === true) {
+            mushroom = 60;
+        }
+        return parseInt(mushroom);
+    }
+    function chicken() {
+        var chicken = 0;
+        var addChicken  = document.getElementById("toppings");
+        if (addChicken .checked === true) {
+            chicken = 100;
+        }
+        return parseInt(mushroom);
+    }
