a0f7a0f13649e932143a8ec966311095b41a13fd
joankamere <joankamere@gmail.com>
2021-03-08 22:38:23 +0300

    save changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+    function getSizeCost() {
+        var selectedSize = document.getElementById("size").value;
+        return parseInt(selectedSize);
+    }
+    
+    function getCrustCost() {
+        var selectedCrust = document.getElementById("crust").value;
+        return parseInt(selectedCrust);
+    }
+    function getNumber() {
+        var selectedNumber = document.getElementById("number").value;
+        return parseInt(selectedNumber);
+
     function mushrooms() {
         var cheese = 0;
         var addCheese = document.getElementById("toppings");
